bb333f673da6ec24eb06e58068191afb73787cd6
Lukas Nichelmann <lukas.nichelmann@web.de>
2024-12-16 22:26:39 +0100

    Refractor to OOD and fix variable scope error

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # GrowController
 Project building a GrowController that measures temperature and humidity. A dehumidifyer can be managed by an active solenoid (coming soon). The project is based on a RaspberryPI 2B and DHT22 sensor .
+
+
+## Running script on bootup
+We use a systemd service in order to start the script as soon as the PI boots up.
+
+1. Create a unit file to define the custom service
+
+```
+sudo nano /lib/systemd/system/GrowController.service
+```
+
+2. Add following text, make sure to replace `user` with your local user
+```
+[Unit]
+Description=My GrowController Service
+After=multi-user.target
+
+[Service]
+Type=idle
+WorkingDirectory=/home/user/Programming/GrowController/SensorBox
+ExecStart=/home/user/Programming/GrowController/env/bin/python3.11 /home/user/Programming/GrowController/SensorBox/humidity.py > /home/user/Programming/logs/GrowController.log 2>&1
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+
+3. Add permissions on the unit file
+```
+sudo chmod 644 /lib/systemd/system/GrowController.service
+```
+
+4. Configure systemd and reboot
+```
+sudo systemctl daemon-reload
+sudo systemctl enable GrowController.service
+sudo reboot
+```
+
+5. To check if the service is running use
+```
+sudo systemctl status GrowController.service
+```
---
+++ SensorBox/GrowControllerV1.py
@@ -0,0 +1,124 @@
+import time
+from datetime import datetime
+import schedule
+import adafruit_dht
+import board
+import thingspeak
+import requests
+import RPi.GPIO  as GPIO
+
+
+class GrowController:
+    def __init__(self,minHumidity,maxHumidity,solenoidPin):
+        self.minHumidity = minHumidity
+        self.maxHumidity = maxHumidity
+        self.dehumidifyerIsOn = False
+        self.solenoidPin = solenoidPin
+        self.WRITE_API = '24V45T2VJ24JLZTE'                                 # put your write API key here, or you´ll push your data to my channel
+        self.BASE_URL = 'https://api.thingspeak.com/update'  
+        self.data = {
+                        'api_key' : self.WRITE_API,
+                        'field1' : 1,
+                        'field2' : 2,
+                        'field3' : 0
+                    }
+        
+        # setup of GPIO
+        GPIO.setmode(GPIO.BCM)                                              # use board specific pin numbering
+        GPIO.setwarnings(False)                                             # ignore warnings
+        GPIO.setup(self.solenoidPin,GPIO.OUT, initial=GPIO.LOW)             # set initial output value low
+
+        # setup DHT
+        print("Starting system..")
+        self.dht_device = adafruit_dht.DHT22(board.D24)                     # GPIO24=pin18 - nr 9 outside from status light on PI 2B
+        time.sleep(2.0)
+        print("System initialized..")
+
+    def setDehumidifyer(self,humidity):
+        if humidity > self.maxHumidity and self.dehumidifyerIsOn == False:
+            GPIO.output(self.solenoidPin, GPIO.HIGH)                        # activate dehumidifyer
+            self.data['field3'] = 1
+            self.dehumidifyerIsOn = True
+        elif humidity < self.minHumidity and self.dehumidifyerIsOn == True:
+            GPIO.output(self.solenoidPin, GPIO.LOW)                         # deactivate dehumidifyer
+            self.data['field3'] = 0
+            self.dehumidifyerIsOn = False 
+
+    def readSensor(self):
+        attempts = 0
+        success = False
+
+        while attempts < 5: # try 5 times
+            try:
+                temperature_c = self.dht_device.temperature
+                humidity = self.dht_device.humidity
+                time.sleep(0.5)
+                print(f"[{datetime.now()}] Temp: {temperature_c:.1f} C    Humidity: {humidity}%")
+                self.setDehumidifyer(humidity)
+                self.data['field1'] = temperature_c
+                self.data['field2'] = humidity
+                response = requests.post(self.BASE_URL,data=self.data)      # post data to ThingSpeak 
+                response.raise_for_status()                                 # throws exception on bad reply
+                success = True
+                break      
+
+            except requests.exceptions.HTTPError as http_err:
+                # Handle HTTP error codes
+                print(f"HTTP error occurred: {http_err}")
+                raise RuntimeError(f"HTTP request failed with status code {response.status_code}") from http_err
+
+            except requests.exceptions.RequestException as req_err:
+                # Handle other possible request errors (e.g., network issues)
+                print(f"Request error occurred: {req_err}")
+                raise RuntimeError("Request failed") from req_err
+            
+            except RuntimeError:
+                attempts += 1
+                success = False
+                print("Error - retrying with attempt nr.", str(attempts+1))
+                time.sleep(0.5)  # Short delay before retrying
+
+        if success == False:
+            GPIO.cleanup()
+            raise RuntimeError("Failed to read sensor data after 5 attempts")
+        
+
+    def schedule_reading(self):
+        # Schedule to run at 00, 15, 30, 45 minutes past every hour
+        schedule.every().hour.at(":00").do(self.readSensor)
+        schedule.every().hour.at(":10").do(self.readSensor)
+        schedule.every().hour.at(":20").do(self.readSensor)
+        schedule.every().hour.at(":30").do(self.readSensor)
+        schedule.every().hour.at(":40").do(self.readSensor)
+        schedule.every().hour.at(":50").do(self.readSensor)
+        print("Scheduled jobs..")
+        # Print the next scheduled job time
+        next_run_time = schedule.next_run()
+        print(f"First job will run at {next_run_time}")
+
+
+        while True:
+            # Run the scheduled jobs
+            schedule.run_pending()
+            time.sleep(15)  # Sleep briefly to prevent excessive CPU usage
+
+        
+
+if __name__ == "__main__":
+    try:
+        GrowController = GrowController(minHumidity=40.0,maxHumidity=55,solenoidPin=18)
+        GrowController.schedule_reading()   # schedule reading
+    except KeyboardInterrupt:
+        print("Shutting down..")
+        GPIO.cleanup()
+
+
+
+
+
+        
+
+
+
+
+
